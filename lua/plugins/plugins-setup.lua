07d4a9f87809367e20da6739bc3583f9420e2f82
ChenXiang <pokersu@live.com>
2023-02-26 20:58:22 +0800

    Update plugins-setup.lua

--- lua/plugins/plugins-setup.lua
+++ lua/plugins/plugins-setup.lua
@@ -56,6 +56,7 @@ return require('packer').startup(function(use)
 
   use "akinsho/bufferline.nvim" -- buffer分割线
   use "lewis6991/gitsigns.nvim" -- 左则git提示
+  use "voldikss/vim-floaterm"
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',  -- 文件检索
