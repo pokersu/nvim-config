bc767d4bc740e9e341faeceb7140c1c0c5c93be9
ChenXiang <pokersu@live.com>
2023-03-10 23:18:37 +0800

    Update keymaps.lua

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -36,3 +36,15 @@ keymap.set("n", "<leader>tn", ":FloatermNext<CR>")
 keymap.set("n", "<leader>tk", ":FloatermKill<CR>")
 keymap.set("t", "<leader>tn", "<C-\\><C-n>:FloatermNext<CR>")
 keymap.set("t", "<leader>te", "<C-\\><C-n>")
+
+
+
+-- jdtls
+keymap.set("n", "<A-o>", "<Cmd>lua require'jdtls'.organize_imports()<CR>")
+
+
+--nnoremap crv <Cmd>lua require('jdtls').extract_variable()<CR>
+--vnoremap crv <Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>
+--nnoremap crc <Cmd>lua require('jdtls').extract_constant()<CR>
+--vnoremap crc <Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>
+--vnoremap crm <Esc><Cmd>lua require('jdtls').extract_method(true)<CR>
